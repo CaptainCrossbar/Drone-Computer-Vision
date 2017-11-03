cda46303bcf431a6edef187f1967853519cde537
Peter Boyle <31522669+CaptainCrossbar@users.noreply.github.com>
2017-11-02 22:43:50 -0400

    face recognize
    program can recognize face and give other information about the face

--- a.py
+++ a.py
@@ -1,2 +1,42 @@
 
-asdasd
+# -*- coding: utf-8 -*-
+import urllib2
+import urllib
+import time
+http_url = 'https://api-us.faceplusplus.com/facepp/v3/detect'
+key = "-HGmh_i6WYTdYNDYFe05-_OVccu-o9TC"
+secret = "2uTc6ZVkxs8LCRIKPzVwoJcyzlQKHrgt"
+filepath = "C:/opt/data/1.jpg"
+boundary = '----------%s' % hex(int(time.time() * 1000))
+data = []
+data.append('--%s' % boundary)
+data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_key')
+data.append(key)
+data.append('--%s' % boundary)
+data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_secret')
+data.append(secret)
+data.append('--%s' % boundary)
+fr=open(filepath,'rb')
+data.append('Content-Disposition: form-data; name="%s"; filename="1.jpg"' % 'image_file')
+data.append('Content-Type: %s\r\n' % 'application/octet-stream')
+data.append(fr.read())
+fr.close()
+data.append('--%s--\r\n' % boundary)
+
+#print(data)
+
+http_body='\r\n'.join(data)
+#buld http request
+req=urllib2.Request(http_url)
+#header
+req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
+req.add_data(http_body)
+try:
+	#post data to server
+	resp = urllib2.urlopen(req, timeout=5)
+	#get response
+	qrcont=resp.read()
+	print(qrcont)
+
+except urllib2.HTTPError as e:
+    print(e.read())
